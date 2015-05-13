79fd4eb440d5f7273b37780b7d2e85e3db86f0b7
Hendrik Cech <hendrik.cech@gmail.com>
2015-05-13 11:26:57 +0200

    changed docker references to dwolter/sparq

--- check.sh
+++ check.sh
@@ -3,7 +3,7 @@ if ! [ -t 0 ]; then
   for arg in "$@"; do
     if [ "$arg" = "-i" ] || [ "$arg" = "--interactive" ]; then
       echo "Start this container with \`-ti\` to run SparQ in interactive mode:"
-      echo "e.g. \`docker run -ti --rm hendrikcech:sparq -i\`"
+      echo "e.g. \`docker run -ti --rm dwolter/sparq -i\`"
       exit
     fi
   done
